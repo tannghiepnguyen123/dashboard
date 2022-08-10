7f18b705b917a5b4ee730412b05fd60f714971d9
Nghiep <tannghiepnguyen123@gmail.com>
2022-08-10 11:19:26 +0700

    finish widgets

---
+++ src/components/features/Features.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./features.scss";
+
+const Features = () => {
+  return <div className="features">Features</div>;
+};
+
+export default Features;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widgets/Widget";
 import "./home.scss";
 
 const Home = () => {
@@ -9,7 +10,13 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        Home container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts"></div>
       </div>
     </div>
   );
